74b1b8e2c4dda55505283fdec88d4759a0526fa4
Dmytro Pashchenko <paschenkod2@gmail.com>
2024-10-07 01:11:23 +0200

    Add editnote command

--- src/bot.ts
+++ src/bot.ts
@@ -6,14 +6,28 @@ export const bot = new Bot(token);
 
 let isGetFile = false;
 let isDeleteNote = false;
+let isEditNote = false;
 
-//сховище для стоврення записки
+//сховище для стоврення та зміни записки
 const memoData = {};
+
+// id для зміни файлу
+let editFileId = 0;
+
+function decodeFilename(contentDisposition) {
+    const match = contentDisposition.match(/filename\*?=['"]?([^;]*)['"]?/);
+    if (match && match[1]) {
+        return decodeURIComponent(match[1].replace(/UTF-8''/, ""));
+    }
+    return "unknown_filename";
+}
 
 // команда для Вітаннячка
 bot.command("start", async (ctx) => {
     isGetFile = false;
     isDeleteNote = false;
+    isEditNote = false;
+    delete memoData[ctx.chat.id];
 
     await ctx.reply("Вітаю! \nЦей Бот допомагає створити службову записку. Для створення натисніть /createnote ");
 });
@@ -21,6 +35,8 @@ bot.command("start", async (ctx) => {
 bot.command("createnote", async (ctx) => {
     isGetFile = false;
     isDeleteNote = false;
+    isEditNote = false;
+    delete memoData[ctx.chat.id];
 
     const keyboard = new InlineKeyboard().text("Службова записка", "service_memo").text("Подання", "submission").text("Звернення", "appeal");
 
@@ -31,6 +47,8 @@ bot.command("createnote", async (ctx) => {
 bot.command("notelist", async (ctx) => {
     isGetFile = false;
     isDeleteNote = false;
+    isEditNote = false;
+    delete memoData[ctx.chat.id];
 
     try {
         const response = await axios.get("https://sr-kpi-api-development.up.railway.app/documents/notes");
@@ -54,13 +72,26 @@ bot.command("notelist", async (ctx) => {
 
 bot.command("getfile", async (ctx) => {
     isDeleteNote = false;
+    isEditNote = false;
+    delete memoData[ctx.chat.id];
 
     isGetFile = true;
     await ctx.reply("Введіть id файлу який ви хочете отримати:");
 });
 
+bot.command("editnote", async (ctx) => {
+    isGetFile = false;
+    isDeleteNote = false;
+    delete memoData[ctx.chat.id];
+
+    isEditNote = true;
+    await ctx.reply("Введіть id файлу який ви хочете змінити:");
+});
+
 bot.command("deletenote", async (ctx) => {
     isGetFile = false;
+    isEditNote = false;
+    delete memoData[ctx.chat.id];
 
     isDeleteNote = true;
     await ctx.reply("Введіть id файлу який ви хочете видалити:");
@@ -84,17 +115,106 @@ bot.callbackQuery("appeal", async (ctx) => {
     await ctx.reply("Введіть одержувача:");
 });
 
+bot.callbackQuery("back", async (ctx) => {
+    const keyboard = new InlineKeyboard().text("Тип файлу", "type").text("Одержувача", "receiver").text("Назву", "title").text("Текст", "content");
+    await ctx.reply("Що ви хочете змінити?", { reply_markup: keyboard });
+});
+
+bot.callbackQuery("type", async (ctx) => {
+    const keyboard = new InlineKeyboard().text("Службова записка", "newServiceMemo").text("Подання", "newSubmission").text("Звернення", "newAppeal").text("Повернутись", "back");
+    await ctx.reply("Оберіть новий тип документу:", { reply_markup: keyboard });
+});
+
+bot.callbackQuery("newServiceMemo", async (ctx) => {
+    try {
+        const response = await axios.patch(
+            "https://sr-kpi-api-development.up.railway.app/documents/note",
+            {
+                id: editFileId,
+                type: "СЛУЖБОВА ЗАПИСКА",
+            },
+            {
+                responseType: "arraybuffer",
+            },
+        );
+
+        const contentDisposition = response.headers["content-disposition"];
+        const filename = decodeFilename(contentDisposition);
+
+        await ctx.reply("Тип файлу було змінено на «Службова записка»:");
+        await ctx.replyWithDocument(new InputFile(Buffer.from(response.data), filename));
+    } catch (error) {
+        await ctx.reply("Помилка при зміні типу файлу на «Службова записка»");
+        await ctx.reply(error);
+    }
+});
+
+bot.callbackQuery("newSubmission", async (ctx) => {
+    try {
+        const response = await axios.patch(
+            "https://sr-kpi-api-development.up.railway.app/documents/note",
+            {
+                id: editFileId,
+                type: "ПОДАННЯ",
+            },
+            {
+                responseType: "arraybuffer",
+            },
+        );
+
+        const contentDisposition = response.headers["content-disposition"];
+        const filename = decodeFilename(contentDisposition);
+
+        await ctx.reply("Тип файлу було змінено на «Подання»:");
+        await ctx.replyWithDocument(new InputFile(Buffer.from(response.data), filename));
+    } catch (error) {
+        await ctx.reply("Помилка при зміні типу файлу на «Подання»");
+        await ctx.reply(error);
+    }
+});
+
+bot.callbackQuery("newAppeal", async (ctx) => {
+    try {
+        const response = await axios.patch(
+            "https://sr-kpi-api-development.up.railway.app/documents/note",
+            {
+                id: editFileId,
+                type: "ЗВЕРНЕННЯ",
+            },
+            {
+                responseType: "arraybuffer",
+            },
+        );
+
+        const contentDisposition = response.headers["content-disposition"];
+        const filename = decodeFilename(contentDisposition);
+
+        await ctx.reply("Тип файлу було змінено на «Звернення»:");
+        await ctx.replyWithDocument(new InputFile(Buffer.from(response.data), filename));
+    } catch (error) {
+        await ctx.reply("Помилка при зміні типу файлу на «Звернення»");
+        await ctx.reply(error);
+    }
+});
+
+bot.callbackQuery("receiver", async (ctx) => {
+    await ctx.reply("Введіть нового одержувача:");
+    memoData[ctx.chat.id] = { step: "newReceiver" };
+});
+
+bot.callbackQuery("title", async (ctx) => {
+    await ctx.reply("Введіть нову назву:");
+    memoData[ctx.chat.id] = { step: "newTitle" };
+});
+
+bot.callbackQuery("content", async (ctx) => {
+    await ctx.reply("Введіть новий текст:");
+    memoData[ctx.chat.id] = { step: "newContent" };
+});
+
 bot.on("message:text", async (ctx) => {
     const chatId = ctx.chat.id;
     const text = ctx.message.text;
-
-    function decodeFilename(contentDisposition) {
-        const match = contentDisposition.match(/filename\*?=['"]?([^;]*)['"]?/);
-        if (match && match[1]) {
-            return decodeURIComponent(match[1].replace(/UTF-8''/, ""));
-        }
-        return "unknown_filename";
-    }
 
     async function createDocument(chatId, ctx: Context) {
         const { recipient, subject, text, type } = memoData[chatId];
@@ -138,6 +258,16 @@ bot.on("message:text", async (ctx) => {
         }
     }
 
+    if (isEditNote) {
+        isEditNote = false;
+
+        editFileId = parseInt(text);
+        // TODO: Check if there is file with such id
+
+        const keyboard = new InlineKeyboard().text("Тип файлу", "type").text("Одержувача", "receiver").text("Назву", "title").text("Текст", "content");
+        await ctx.reply("Що ви хочете змінити?", { reply_markup: keyboard });
+    }
+
     if (isDeleteNote) {
         isDeleteNote = false;
 
@@ -168,6 +298,81 @@ bot.on("message:text", async (ctx) => {
             await createDocument(chatId, ctx);
             await ctx.reply("Записка створена!");
             delete memoData[chatId];
+            break;
+        case "newReceiver":
+            delete memoData[ctx.chat.id];
+            try {
+                const response = await axios.patch(
+                    "https://sr-kpi-api-development.up.railway.app/documents/note",
+                    {
+                        id: editFileId,
+                        receiver: text,
+                    },
+                    {
+                        responseType: "arraybuffer",
+                    },
+                );
+
+                const contentDisposition = response.headers["content-disposition"];
+                const filename = decodeFilename(contentDisposition);
+
+                await ctx.reply(`Одержувача було змінено на «${text}»:`);
+                await ctx.replyWithDocument(new InputFile(Buffer.from(response.data), filename));
+            } catch (error) {
+                await ctx.reply(`Помилка при зміні одержувача на «${text}»`);
+                await ctx.reply(error);
+            }
+
+            break;
+        case "newTitle":
+            delete memoData[ctx.chat.id];
+            try {
+                const response = await axios.patch(
+                    "https://sr-kpi-api-development.up.railway.app/documents/note",
+                    {
+                        id: editFileId,
+                        title: text,
+                    },
+                    {
+                        responseType: "arraybuffer",
+                    },
+                );
+
+                const contentDisposition = response.headers["content-disposition"];
+                const filename = decodeFilename(contentDisposition);
+
+                await ctx.reply(`Назву було змінено на «${text}»:`);
+                await ctx.replyWithDocument(new InputFile(Buffer.from(response.data), filename));
+            } catch (error) {
+                await ctx.reply(`Помилка при зміні назви на «${text}»`);
+                await ctx.reply(error);
+            }
+
+            break;
+        case "newContent":
+            delete memoData[ctx.chat.id];
+            try {
+                const response = await axios.patch(
+                    "https://sr-kpi-api-development.up.railway.app/documents/note",
+                    {
+                        id: editFileId,
+                        content: text,
+                    },
+                    {
+                        responseType: "arraybuffer",
+                    },
+                );
+
+                const contentDisposition = response.headers["content-disposition"];
+                const filename = decodeFilename(contentDisposition);
+
+                await ctx.reply(`Текст було змінено на «${text}»:`);
+                await ctx.replyWithDocument(new InputFile(Buffer.from(response.data), filename));
+            } catch (error) {
+                await ctx.reply(`Помилка при зміні тексту на «${text}»`);
+                await ctx.reply(error);
+            }
+
             break;
     }
 });
